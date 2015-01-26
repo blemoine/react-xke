a849f2d3db5397a0de4d79b267a1b84290a11c17
Jeremy Vinai <jvinai@xebia.fr>
2015-01-26 14:19:09 +0100

    Creation de la branche step one

--- scripts/app.jsx
+++
@@ -1,5 +0,0 @@
-var React = require('react');
-var Header = require('./components/Header.jsx');
-var Bars = require('./components/Bars.jsx');
-
-React.render(<div><Header /><Bars /></div>, document.getElementById('react'));
